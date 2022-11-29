d2f08c82f1065028543c89d3ecb400d9095b8c31
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-29 12:11:14 +0100

    refactor: fix imports

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
 
 import Navigation from "../Navigation";
-import LandingPage from "../Landing";
-import SignUpPage from "../SignUp";
-import SignInPage from "../SignIn";
-import PasswordForgetPage from "../PasswordForget";
-import HomePage from "../Home";
-import AccountPage from "../Account";
-import AdminPage from "../Admin";
+import LandingPage from "../../views/Landing";
+import SignUpPage from "../../views/SignUp";
+import SignInPage from "../../views/SignIn";
+import PasswordForgetPage from "../../views/PasswordForget";
+import HomePage from "../../views/Home";
+import AccountPage from "../../views/Account";
+import AdminPage from "../../views/Admin";
 
 const App = () => {
   return (
