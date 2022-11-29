a925b52850bb0cceda79105e191680a12831eb00
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-29 12:11:58 +0100

    refactor: move components to pages and rm duplicates

--- src/components/Firebase/index.js
+++
@@ -1,5 +0,0 @@
-import { FirebaseProvider } from "./context";
-import Firebase from "./firebase";
-
-export default Firebase; 
-export { FirebaseProvider };
--- src/components/PasswordChange/index.js
+++
@@ -1,7 +0,0 @@
-const PasswordChange = () => {
-    return (
-        <></>
-    );
-};
-
-export default PasswordChange;
